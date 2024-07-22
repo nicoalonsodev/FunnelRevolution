e33359b44f2c65ca5aa3809eeda622b3e82cab86
NAlonsoBulacio <nicoalonso99.na@gmail.com>
2024-07-22 15:40:07 -0300

    button change

--- frontend/src/components/AnimatedButton/AnimatedButton.jsx
+++ frontend/src/components/AnimatedButton/AnimatedButton.jsx
@@ -7,9 +7,9 @@ const AnimatedButton = () => {
       href="https://www.instagram.com"
       target="_blank"
       rel="noopener noreferrer"
-      className="w-full lg:w-2/4 animated-button text-xl"
+      className="w-full lg:w-2/4 animated-button text-xl border-[2px] border-[#dc9c35]"
     >
-      <span className="text">AGENDA TU LLAMADA</span>
+      <span className="text-lg lg:text-2xl">AGENDA TU LLAMADA</span>
       <span className="subtext text-sm lg:text-lg text-balance">
         Y comienza a implementar este nuevo modelo de negocio ahora
       </span>
