9225e3c8c0900b1c216c031d18157f81dbbc498c
NAlonsoBulacio <nicoalonso99.na@gmail.com>
2024-08-15 21:05:33 -0300

    testing raul

--- frontend/src/views/RaulCrisorio/Afiliados/MariaJoseMedina/LandingVslMaria.jsx
+++ frontend/src/views/RaulCrisorio/Afiliados/MariaJoseMedina/LandingVslMaria.jsx
@@ -24,22 +24,25 @@ const LandingVslMaria = () => {
     backgroundPosition: "center",
     backgroundRepeat: "no-repeat",
   };
-  const historyPart1 =  [
-    "Todo empezó a los 25 años, recién egresado y con un trabajo estable, me sentía perdido. Mi sueldo se iba en gastos innecesarios, fiestas y alcohol, afectando mi salud",
-    "Mis dos pasiones siempre han sido dormir y comer bien. En agosto de 2018, una colega posteó 'Gané $4 mientras dormía', y me intrigó. La contacté esperando que trabajara en una empresa de colchones, pero era mucho mejor.",
-    "Vi un gráfico y oí hablar de trading por primera vez. Conversamos tres horas, salí maravillado y esperanzado, sabiendo que tenía una gran oportunidad para cambiar mi vida y la de mi familia. Y así fue.",
-  ]
-  const historyPart2 =  [
-    "Fue un proceso largo desde el 2018 hasta hoy, pero no me arrepiento de absolutamente nada, logre conocer muchas personas, grandes mentores, conocí millonarios, gane sumas de dinero que nunca antes había conocido. Hoy en día conozco más de 20 países, puedo elegir el lugar en donde vivir y gozo de la libertad diaria de elegir que quiero hacer todos los días.",
-    "Puedo pasar tiempo con mi familia, ver a mi hija crecer todos los días, rodearme de personas que ganan millones de dólares al año y que todo el tiempo hablan de prosperidad y negocios.",
-    "No te voy a negar, cuando comencé pensé que esto me haría millonario de la noche a la mañana, y nada más alejado de la realidad. Comprendí después de varios años de proceso, que en el mundo de los negocios hay que tener una mirada a largo plazo. Es la única forma de tener éxito duradero. Y que los atajos no existen, había que hacer las cosas bien.",
-  ]
-  const historyPart3 =  [
-    "Hoy gracias a todos mis aciertos y desaciertos en el mundo de los negocios, puedo elegir Revolution como vehículo financiero más potente que existe hoy en día. Porque está constituido desde bases sólidas, pensado a largo plazo y combina modelos de negocios super potentes.",
-    "Hoy veo a mis amigos de la facultad y el colegio, en un trabajo que no les gusta, manejando un auto que no les gusta, si es que tienen, viviendo en una ciudad que no les gusta, y trabajando todo el año porque no les alcanza para irse de vacaciones.",
-    "Es una locura, asumen que todo lo que les pasa es normal, y dejame de decirte, que eso NO es normal, tienes todas las capacidades para vivir la vida que vos elijas, no la que te toca.",
-    "Solo tienes que tener huevos para tomar la decisión de hacerlo, y huevos para mantenerte.",
-  ]
+const historyPart1 = [
+  "Todo empezó a los 25 años, recién egresado y con un trabajo estable, me sentía perdido. Mi sueldo se iba en <strong>gastos innecesarios</strong>, <strong>fiestas</strong> y <strong>alcohol</strong>, afectando mi <strong>salud</strong>",
+  "Mis dos pasiones siempre han sido <strong>dormir</strong> y <strong>comer bien</strong>. En agosto de 2018, una colega posteó 'Gané $4 mientras <strong>dormía</strong>', y me intrigó. La contacté esperando que trabajara en una empresa de <strong>colchones</strong>, pero era mucho mejor.",
+  "Vi un <strong>gráfico</strong> y oí hablar de <strong>trading</strong> por primera vez. Conversamos tres horas, salí maravillado y esperanzado, sabiendo que tenía una gran oportunidad para cambiar mi <strong>vida</strong> y la de mi <strong>familia</strong>. Y así fue.",
+]
+
+const historyPart2 = [
+  "Fue un proceso largo desde el 2018 hasta hoy, pero no me arrepiento de absolutamente nada, logré conocer muchas personas, grandes <strong>mentores</strong>, conocí <strong>millonarios</strong>, gané sumas de <strong>dinero</strong> que nunca antes había conocido. Hoy en día conozco más de <strong>20 países</strong>, puedo elegir el lugar en donde vivir y gozo de la <strong>libertad diaria</strong> de elegir qué quiero hacer todos los días.",
+  "Puedo pasar tiempo con mi <strong>familia</strong>, ver a mi <strong>hija</strong> crecer todos los días, rodearme de personas que ganan <strong>millones de dólares</strong> al año y que todo el tiempo hablan de <strong>prosperidad</strong> y <strong>negocios</strong>.",
+  "No te voy a negar, cuando comencé pensé que esto me haría <strong>millonario</strong> de la noche a la mañana, y nada más alejado de la realidad. Comprendí después de varios años de proceso, que en el mundo de los <strong>negocios</strong> hay que tener una mirada a <strong>largo plazo</strong>. Es la única forma de tener <strong>éxito duradero</strong>. Y que los <strong>atajos</strong> no existen, había que hacer las cosas bien.",
+]
+
+const historyPart3 = [
+  "Hoy gracias a todos mis aciertos y desaciertos en el mundo de los <strong>negocios</strong>, puedo elegir <strong>Revolution</strong> como vehículo financiero más potente que existe hoy en día. Porque está constituido desde bases sólidas, pensado a <strong>largo plazo</strong> y combina <strong>modelos de negocios</strong> super potentes.",
+  "Hoy veo a mis amigos de la <strong>facultad</strong> y el <strong>colegio</strong>, en un trabajo que no les gusta, manejando un <strong>auto</strong> que no les gusta, si es que tienen, viviendo en una <strong>ciudad</strong> que no les gusta, y trabajando todo el año porque no les alcanza para irse de <strong>vacaciones</strong>.",
+  "Es una locura, asumen que todo lo que les pasa es <strong>normal</strong>, y déjame decirte, que eso NO es <strong>normal</strong>, tienes todas las capacidades para vivir la <strong>vida</strong> que vos elijas, no la que te toca.",
+  "Solo tienes que tener <strong>huevos</strong> para tomar la decisión de hacerlo, y <strong>huevos</strong> para mantenerte.",
+]
+
 const actually = [
   "Gané más de $500.000, vivo en Buenos Aires y tengo amigos que comparten los mismos objetivos.",
   "El dinero ya no es un problema.",
