03b01f0ed21266d8a82c2f240e8ecae00ce78219
NAlonsoBulacio <nicoalonso99.na@gmail.com>
2024-08-24 15:31:32 -0300

    fernando migliano

--- frontend/src/views/FernandoDamian/Afiliados/PabloCastellon/LandingVslPablo.jsx
+++ frontend/src/views/FernandoDamian/Afiliados/PabloCastellon/LandingVslPablo.jsx
@@ -14,10 +14,10 @@ import ModalButton from "../../../../components/ModalButton/ModalButton";
 import { Link } from "react-router-dom";
 
 
-import img1 from "../../../../assets/cristian_castellon/1.webp";
-import img2 from "../../../../assets/cristian_castellon/2.webp";
-import img3 from "../../../../assets/cristian_castellon/3.webp";
-import img4 from "../../../../assets/cristian_castellon/4.webp";
+import img1 from "../../../../assets/fernando_damian/1.webp";
+import img2 from "../../../../assets/fernando_damian/2.webp";
+import img3 from "../../../../assets/fernando_damian/3.webp";
+import img4 from "../../../../assets/fernando_damian/4.webp";
 
 const LandingVslPablo = () => {
   const background2Style = {
@@ -26,34 +26,34 @@ const LandingVslPablo = () => {
     backgroundPosition: "center",
     backgroundRepeat: "no-repeat",
   };
-const historyPart1 = [
-  "Siempre me llamó la atención el mundo del <strong>emprendimiento</strong>, los <strong>negocios</strong> y la formación de <strong>empresas</strong>. Me recibí de <strong>Técnico en administración de empresas</strong>, estudié 1 año en la <strong>UBA</strong> y 2 años en la <strong>UADE</strong>. Trabajé un año en <strong>IBM</strong>, y eso fue más que suficiente para darme cuenta que el <strong>sistema tradicional</strong> no me iba a dar la <strong>vida</strong> que quería.",
-  "Ahí inició mi carrera como <strong>emprendedor</strong>, a los 21 años compré un <strong>auto</strong> a crédito para ponerlo a hacer <strong>UBER</strong>, avancé con un <strong>local de comida</strong>, después llegó a mi vida el mundo de los <strong>negocios digitales</strong>. Me presentaron la empresa <strong>AMWAY</strong>, que es una empresa <strong>Norteamericana</strong> que lleva más de 60 años en funcionamiento, donde vos podrías distribuir sus <strong>productos</strong>, comisionar por tus <strong>ventas</strong> y armar <strong>equipos de trabajo</strong>. Gracias a esa actividad conocí más de <strong>20 países</strong> y gané mucho <strong>dinero</strong> durante 5 años. Después armé una empresa de <strong>ventas de planes de autos</strong> con 2 amigos, puse una <strong>oficina</strong> con personas haciendo <strong>arbitraje</strong>, armé una <strong>comunidad de cripto players</strong> en <strong>AXIE INFINITY</strong>, pero no me fue tan bien como planeaba...",
-]
-
-const historyPart2 = [
-  "Después me empezó a llamar la atención el <strong>Trading</strong> y fue ahí cuando conocí a <strong>REVOLUTION</strong> y <strong>Cristian Diaz</strong>, y me encantó, en el minuto uno supe que acá sí podía hacer <strong>carrera</strong> y dedicar años de mi vida a convertirme en un <strong>profesional</strong> e impactar de forma positiva a más personas.",
-  "Me encanta ponerme <strong>metas</strong> y trabajar con <strong>personas</strong> así que cuando entendí lo potente del <strong>trading</strong> y el <strong>plan de mercadeo</strong> lo empecé a escalar, mis primeros 5 meses me costó un poco superar los <strong>usd 500 de ganancias mensuales</strong>, hoy en día soy un <strong>trader rentable</strong> y el principal <strong>creador de equipos</strong> de <strong>REVOLUTION</strong>, gano más de <strong>usd 30.000 al mes</strong>, vivo en una <strong>casa soñada</strong>, viajo seguido, ayudo económicamente a mis <strong>viejos</strong> para que no les falte nada, y lo mejor de todo es que hay cientos de personas en el <strong>equipo</strong> ganando <strong>cientos y miles de dólares extras</strong> al mes gracias a tomar la decisión de emprender con nosotros en <strong>REVOLUTION</strong>.",
-]
-
-const historyPart3 = [
-  "Te invitamos a dar el <strong>paso</strong>, anímate a hacer algo <strong>distinto</strong>, tenemos un <strong>método</strong> que funciona, que ya fue testeado, y constantemente lo estamos <strong>mejorando</strong>.",
-  "Si vos das tu <strong>100%</strong> vas a poder <strong>progresar</strong> y hacer <strong>carrera</strong> con nosotros. Lo <strong>tradicional</strong> ya no funciona, el mundo cambió, las <strong>oportunidades</strong> están, hay personas que las aprovechan y otras no! ¡Animate! ¡Nosotros te ayudamos!",
-]
-const actually = [
-  "Facturé más de 200.000 USD con Revolution, vivo en Argentina y tengo amigos que comparten los mismos objetivos.",
-  "El dinero ya no es un problema.",
-  "Puedo cuidar de mí mismo, de mi familia, de mis amigos y, lo más importante…",
-  "Puedo aportarles a personas que son como yo.",
-  "Y ahora seguramente te estarás preguntando, ¿cómo es que este joven argentino creció tanto y tan rápido con Revolution?",
- ` Acá los llamamos "los tres pilares" de este nuevo modelo de negocio…`,
-]
-  const historyData = [
-   "Cristian Agustín Castellón",
-    "Argentina",
-    "5 años",
+  const historyPart1 = [
+    "Soy Fernando Damián Migliano, tengo 41 años y soy instructor de musculación y culturismo. Desde los 18 años, siempre he sido un emprendedor independiente.",
+    "Comencé vendiendo productos y luego expandí mis negocios a un kiosco y un local de ropa en Buenos Aires. Con el tiempo, me convertí en agente inmobiliario y adquirí propiedades en Miami.",
   ]
-
+  
+  const historyPart2 = [
+    "En 2013, lancé una tienda de ropa online llamada 'Let Me Shine'. Luego, en 2023, descubrí Revolution a través de un anuncio en Facebook.",
+    "En solo seis meses, ya estaba generando más de 500 USD al mes haciendo trading desde mi casa.",
+    "Esto me llevó a formar parte del staff de Revolution, ayudando a más de 20 personas a transformar sus vidas y dejar sus empleos tradicionales."
+  ]
+  
+  const historyPart3 = [
+    "Hoy, después de 18 meses, genero un promedio de 10.000 USD al mes. ",
+    "Revolution ha cambiado mi vida y ahora tengo la libertad financiera que siempre soñé.",
+  ]
+  const actually = [
+   
+  " Si estás buscando una oportunidad para cambiar tu vida, este es tu momento.",
+    "Puedo cuidar de mí mismo, de mi familia, de mis amigos y, lo más importante…",
+    "Puedo aportarles a personas que son como yo.",
+    "Y ahora seguramente te estarás preguntando, ¿cómo es que este joven argentino creció tanto y tan rápido con Revolution?",
+   ` Acá los llamamos "los tres pilares" de este nuevo modelo de negocio…`,
+  ]
+    const historyData = [
+     "Fernando Migliano",
+      "Buenos Aires",
+      "2 años",
+    ]
   const calendlyPage = "/pablo-castellon/calendly";
 const historyImages = [ img1, img2, img3, img4]
     const isRegistered = localStorage.getItem("isRegistered") === "true";
